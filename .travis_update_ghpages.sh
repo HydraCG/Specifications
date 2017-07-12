d7b77aeb82e0f67803943a481e040577319a3d8a
Markus Lanthaler <mark_lanthaler@gmx.net>
2017-07-12 09:13:44 +0200

    Fix GitHub pages updated script

--- .travis_update_ghpages.sh
+++ .travis_update_ghpages.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
+
+set -o errexit -o nounset
+
 # Only publish from the main repository's master branch
-REPO_NAME="HydraCG/Specifications"
-if [ "$TRAVIS_REPO_SLUG" != "$REPO_NAME" ] || [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then exit; fi
-echo -e "Generating gh-pages...\n"
+# REPO_NAME="HydraCG/Specifications"
+# if [ "$TRAVIS_REPO_SLUG" != "$REPO_NAME" ] || [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#   echo "This commit was made against $TRAVIS_BRANCH and not the master. Don't generate gh-pages."
+#   exit;
+# fi
+
+echo "Updating gh-pages..."
+
+echo "Generated files"
+pwd
+ls drafts/diagram
 
 # Checkout the gh-pages branch
 REPO_PATH=$PWD
 pushd $HOME
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$REPO_NAME gh-pages 2>&1 > /dev/null
-cd gh-pages
 
-# Don't update if already at the latest version
-if [[ `git log -1 --pretty=%B` == *$TRAVIS_COMMIT* ]]; then exit; fi
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$REPO_NAME gh-pages
+git config user.name  "Travis"
+git config user.email "travis@hydra-cg.com"
+
+cd gh-pages
+pwd
+mkdir -p drafts/diagram/
 
 # Update documents
-cp -r $REPO_PATH .
+cp -r $REPO_PATH/drafts/diagram/* drafts/diagram/
 
 # Commit and push latest version
-git add .
-git config user.name  "Travis"
-git config user.email "travis@travis-ci.org"
+git add -A .
+git status
 git commit -m "Update to $TRAVIS_COMMIT."
-git push -fq origin gh-pages 2>&1 > /dev/null
+git push -q origin gh-pages 2>&1 > /dev/null
+
 popd
